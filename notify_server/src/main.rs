ecdebcaece467664889a854e768db67d411923d0
0x0918 <yiyunbest@gmail.com>
2024-07-17 16:46:17 +0800

    feature: refactor code to a workspace and start notify server

--- notify_server/src/main.rs
+++ notify_server/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use axum::serve;
-use chat::{get_router, AppConfig};
+use notify_server::get_router;
 use tokio::net::TcpListener;
 use tracing::{info, level_filters::LevelFilter};
 use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
@@ -10,14 +10,10 @@ async fn main() -> Result<()> {
     let layer = Layer::new().with_filter(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
 
-    let config = AppConfig::load()?;
-    let addr = format!("0.0.0.0:{}", config.server.port);
-
-    let app = get_router(config);
-    let listener = TcpListener::bind(&addr).await?;
-    info!("Listening on: {}", addr);
-
+    let addr = "0.0.0.0:6687";
+    let app = get_router();
+    let listener = TcpListener::bind(addr).await?;
+    info!("Listening on :{}", addr);
     serve(listener, app.into_make_service()).await?;
-
     Ok(())
 }
