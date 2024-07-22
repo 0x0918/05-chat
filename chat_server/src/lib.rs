3ad404b7762c9d7769fa4cb3a159e2a42b1a0f9d
0x0918 <yiyunbest@gmail.com>
2024-07-22 13:50:01 +0800

    feature: learned axum middleware and finished auth middleware

--- chat_server/src/lib.rs
+++ chat_server/src/lib.rs
@@ -1,6 +1,7 @@
 mod config;
 mod error;
 mod handlers;
+mod middlewares;
 mod models;
 mod utils;
 
@@ -8,12 +9,15 @@ use std::{fmt::Debug, ops::Deref, sync::Arc};
 
 use anyhow::{Context, Result};
 use axum::{
+    middleware::from_fn_with_state,
     routing::{get, patch, post},
     Router,
 };
+
 pub use config::*;
 pub use error::*;
 pub use handlers::*;
+pub use middlewares::*;
 pub use models::*;
 use sqlx::PgPool;
 use utils::{DecodingKey, EncodingKey};
@@ -36,8 +40,6 @@ pub async fn get_router(config: AppConfig) -> Result<Router, AppError> {
     let state = AppState::try_new(config).await?;
 
     let api = Router::new()
-        .route("/signin", post(signin_handler))
-        .route("/signup", post(signup_handler))
         .route("/chat", get(list_chat_handler).post(create_chat_handler))
         .route(
             "/chat/:id",
@@ -45,13 +47,16 @@ pub async fn get_router(config: AppConfig) -> Result<Router, AppError> {
                 .delete(delete_chat_handler)
                 .post(send_message_handler),
         )
-        .route("/chat/:id/messages", get(list_message_handler));
+        .route("/chat/:id/messages", get(list_message_handler))
+        .layer(from_fn_with_state(state.clone(), verify_token))
+        .route("/signin", post(signin_handler))
+        .route("/signup", post(signup_handler));
 
     let app = Router::new()
         .route("/", get(index_handler))
         .nest("/api", api)
         .with_state(state);
-    Ok(app)
+    Ok(set_layer(app))
 }
 
 impl Deref for AppState {
